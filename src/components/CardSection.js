f3cd6f3b45f1da8656c9ebf4d1be1a5fa3debe74
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-10-16 20:09:58 -0300

    updade

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,22 +1,26 @@
 import stylesCard from '../components/Card.module.css';
 import styles from '../components/CardSection.module.css';
 import CardItem from './CardItem';
-import { FaGoogle, FaPython } from 'react-icons/fa';
+import { FaPython } from 'react-icons/fa';
 import DelphiLogo from '../img/icon-delphi.png';
 import { SiDjango } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
+import { TbSql } from "react-icons/tb";
 
 
 export default function CardSection(){
     return(
         <div className={styles.CardSectionContainer} >
+            <div className={styles.CardSectionHeaderText} >
+                <h2>Tecnologias</h2>
+            </div>
             <div className={styles.CardSectionContent} >
                 <div className={styles.CardSectionCollumnLeft} >
                     <CardItem
                         icon={DelphiLogo}
                         styleItemPhoto='CardItemStyleDelphi'
                         textHeader='Delphi'
-                        textLeft='Junior'
+                        textLeft='Language'
                         textRight='Now'
                     />
                     <CardItem
@@ -26,22 +30,28 @@ export default function CardSection(){
                         textLeft='Framework'
                         textRight='2023'    
                     />
-                    
-                </div>
-                <div className={styles.CardSectionCollumnRight} >
-                    <CardItem
-                        icon={IoLogoJavascript}
-                        styleItemPhoto='CardItemStyleJavascript'
-                        textHeader='Javascript'
-                        textLeft='Language'
-                        textRight='2024'
-                    />
                     <CardItem
                         icon={FaPython}
                         styleItemPhoto='CardItemStylePython'
                         textHeader='Python'
                         textLeft='Language'
                         textRight='2023'    
+                    />
+                </div>
+                <div className={styles.CardSectionCollumnRight} >
+                    <CardItem
+                        icon={TbSql}
+                        styleItemPhoto='CardItemStyleSQL'
+                        textHeader='SQL'
+                        textLeft='Language'
+                        textRight='2024'
+                    />
+                    <CardItem
+                        icon={IoLogoJavascript}
+                        styleItemPhoto='CardItemStyleJavascript'
+                        textHeader='Javascript'
+                        textLeft='Language'
+                        textRight='2024'
                     />
                 </div>
             </div>
