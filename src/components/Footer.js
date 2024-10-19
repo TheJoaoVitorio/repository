981293fe8efe3e6ebec1c99e3ba82feb6749c783
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-10-18 22:30:43 -0300

    footer comlpeted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import styles from '../components/Footer.module.css'
+import { FaGithub, FaReact } from "react-icons/fa";
+import Lottie from 'lottie-react'; 
+import LottieReactIcon from '../lotties/Animation - 1729298523997.json'
+import { style } from 'framer-motion/client';
+
 
 export default function Footer(){
     return(
         <footer className={styles.FooterContainer} >
-            <h1>Footer</h1>
+            <div className={styles.FooterContentText} >
+                <div className={styles.FooterTextLeft} >
+                    <h2>Portifolio by  <span>João Vitório</span> </h2>
+                </div>
+                <div className={styles.FooterTextRight}  >
+                    <a className={styles.FooterTextGitHub}  href='https://github.com/TheJoaoVitorio' >
+                        <FaGithub/>
+                    </a>
+                    <div className={styles.BadgeReact} >
+                        <div className={styles.BadgeReactText} >
+                            <h2>Made with </h2> 
+                        </div>
+                        <div className={styles.BadgeIconReact} > 
+                        <Lottie 
+                            animationData={LottieReactIcon} // Arquivo JSON da animação
+                            loop={true} 
+                            style={{ width: 30, height: 30 }} // Estilo da animação
+                        />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
